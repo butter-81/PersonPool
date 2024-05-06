e70c36709540b1cd402c7add85ed5428d284bb39
butter-81 <kimbutter@sookmyung.ac.kr>
2024-05-06 23:08:49 +0900

    포트번호 주석처리

--- server.js
+++ server.js
@@ -1,13 +1,63 @@
 // express 불러오기 및 사용하기
 const express = require("express");
+const cors = require('cors');
+const mysql = require('mysql2');
+const path = require('path');
 const app = express();
-const port = 3306;
+require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 
-//http 서버 실행
-app.listen(port, () => {
-    console.log("서버 정상적으로 실행됨");
-})
+
+app.use(
+    cors({
+        origin: '*',
+    })
+)
+
+// const Sequelize = require('sequelize');
+// const sequelize = new Sequelize('nelp', )
+
+
+const dbUrl = process.env.DB_URL;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PW;
+const dbName = process.env.DATABASE;
+const port = process.env.PORT;
+
+//console.log(`${dbUrl}, ${dbUser}, ${dbPassword}, ${dbName}`);
+
+
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
+
+const connection = mysql.createConnection ({
+    host: dbUrl,            // 데이터베이스 호스트
+    user: dbUser,           // 데이터베이스 사용자 이름  
+    password: dbPassword, //데이터베이스 비밀번호
+    database: dbName, // 연결할 데이터베이스 이름
+    //port: '3306',
+});
+
+const qs = require('qs');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+
+app.use(express.json());
+app.use(bodyParser.json());
+
+connection.connect((err) => {
+    if (err) {
+        return console.error('[Mysql 연결 에러] error: \n' + err.message);
+    } else {
+        app.listen(port, () => {
+            console.log('MYSQL 연결 성공!');
+            console.log('PersonPool 서버 8080 포트 연결 성공!');
+        });
+    }
+});
 
 app.get("/", (req, res) => {
-    response.send("성공");
+    response.send("index 페이지 접속");
 })
